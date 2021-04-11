adc6e1f3646eaa8605d00f78a6c521e1909ed52c
Vlad <longsno@gmail.com>
2021-04-11 16:35:00 +0300

    feat: pot done

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,41 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const newMatrix = [];
+  for (let i = 0; i < matrix.length; i++) {
+    const line = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      let count = 0;
+      if (matrix[i][j + 1]) {
+        count++;
+      }
+      if (matrix[i][j - 1]) {
+        count++;
+      }
+      if (matrix[i - 1] && matrix[i - 1][j]) {
+        count++;
+      }
+      if (matrix[i + 1] && matrix[i + 1][j]) {
+        count++;
+      }
+      if (matrix[i - 1] && matrix[i - 1][j + 1]) {
+        count++;
+      }
+      if (matrix[i + 1] && matrix[i + 1][j + 1]) {
+        count++;
+      }
+      if (matrix[i - 1] && matrix[i - 1][j - 1]) {
+        count++;
+      }
+      if (matrix[i + 1] && matrix[i + 1][j - 1]) {
+        count++;
+      }
+      line.push(count);
+    }
+    newMatrix.push(line);
+  }
+  return newMatrix;
 }
 
 module.exports = minesweeper;
